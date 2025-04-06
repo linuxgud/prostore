23da02327e70683a228fa8bad039f77b560aa40e
Michal Kment <gitlab.zjqud@aleeas.com>
2025-04-06 12:48:38 +0200

    persist session cart

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { compareSync } from 'bcrypt-ts-edge'
 import type { NextAuthConfig } from 'next-auth'
 import { authConfig } from './auth.config'
+import { cookies } from 'next/headers'
 
 export const config = {
   pages: {
@@ -60,6 +61,7 @@ export const config = {
     },
     async jwt({ token, user, trigger, session }: any) {
       if (user) {
+        token.id = user.id
         token.role = user.role
         if (user.name === 'NO_NAME') {
           token.name = user.email!.split('@')[0]
@@ -67,6 +69,24 @@ export const config = {
             where: { id: user.id },
             data: { name: token.name },
           })
+        }
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies()
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            })
+            if (sessionCart) {
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              })
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              })
+            }
+          }
         }
       }
       return token
