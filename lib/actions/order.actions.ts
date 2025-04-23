d0a08eb556ea496b8e4213a854a3c6ec3112a879
Michal Kment <gitlab.zjqud@aleeas.com>
2025-04-23 09:06:41 +0200

    create paypal order action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,7 +7,9 @@ import { getMyCart } from './cart.actions'
 import { getUserById } from './user.actions'
 import { insertOrderSchema } from '../validators'
 import { prisma } from '@/db/prisma'
-import { CartItem } from '@/types'
+import { CartItem, PaymentResult } from '@/types'
+import { paypal } from '../paypal'
+import { revalidatePath } from 'next/cache'
 
 export async function createOrder() {
   try {
@@ -104,4 +106,40 @@ export async function getOrderById(orderId: string) {
   })
 
   return convertToPlainObject(data)
+}
+
+// Create new paypal order
+export async function createPayPalOrder(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: { id: orderId },
+    })
+
+    if (order) {
+      const paypalOrder = await paypal.createOrder(Number(order.totalPrice))
+      await prisma.order.update({
+        where: { id: orderId },
+        data: {
+          paymentResult: {
+            id: paypalOrder.id,
+            email_address: '',
+            status: '',
+            pricePaid: 0,
+          },
+        },
+      })
+      return {
+        success: true,
+        message: 'Item order created successfully',
+        data: paypalOrder.id,
+      }
+    } else {
+      throw new Error('Order not found')
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    }
+  }
 }
--- types/index.ts
+++ types/index.ts
@@ -6,7 +6,7 @@ import {
   shippingAddressSchema,
   insertOrderItemSchema,
   insertOrderSchema,
-  //paymentResultSchema,
+  paymentResultSchema,
   //insertReviewSchema,
 } from '@/lib/validators'
 
@@ -32,7 +32,7 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   user: { name: string; email: string }
   //paymentResult: PaymentResult;
 }
-// export type PaymentResult = z.infer<typeof paymentResultSchema>;
+export type PaymentResult = z.infer<typeof paymentResultSchema>
 // export type Review = z.infer<typeof insertReviewSchema> & {
 //   id: string;
 //   createdAt: Date;
