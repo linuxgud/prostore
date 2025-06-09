78749fcbbb87610e03c3a4648998f03ab310566f
Michal Kment <gitlab.zjqud@aleeas.com>
2025-06-09 21:16:30 +0200

    delete products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,8 +2,9 @@
 
 //import { PrismaClient } from '@prisma/client'
 import { prisma } from '@/db/prisma'
-import { convertToPlainObject } from '@/lib/utils'
+import { convertToPlainObject, formatError } from '@/lib/utils'
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '@/lib/constants'
+import { revalidatePath } from 'next/cache'
 
 // Get latest products
 export async function getLatestProducts() {
@@ -43,5 +44,28 @@ export async function getAllProducts({
   return {
     data,
     totalPages: Math.ceil(dataCount / limit),
+  }
+}
+
+// Delete a product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    })
+    if (!productExists) throw new Error('Product not found')
+    await prisma.product.delete({
+      where: { id },
+    })
+    revalidatePath('/admin/products')
+    return {
+      success: true,
+      message: 'Product deleted successfully',
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    }
   }
 }
