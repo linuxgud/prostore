9e224405f40dc8ec20a159d8b3e31176314f78ad
Michal Kment <gitlab.zjqud@aleeas.com>
2025-06-09 09:06:52 +0200

    get products for admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,7 +3,7 @@
 //import { PrismaClient } from '@prisma/client'
 import { prisma } from '@/db/prisma'
 import { convertToPlainObject } from '@/lib/utils'
-import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants'
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '@/lib/constants'
 
 // Get latest products
 export async function getLatestProducts() {
@@ -20,4 +20,28 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   })
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string
+  limit?: number
+  page: number
+  category?: string
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  })
+
+  const dataCount = await prisma.product.count()
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  }
 }
