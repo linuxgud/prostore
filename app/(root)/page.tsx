bdd1ea1de201fe1ec35a0c28fdecb6a93f83ea91
Michal Kment <gitlab.zjqud@aleeas.com>
2025-06-22 16:27:51 +0200

    featured product carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,21 @@
 
 import ProductList from '@/components/shared/product/product-list'
 //import sampleData from '@/db/sample-data'
-import { getLatestProducts } from '@/lib/actions/product.actions'
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions'
+import ProductCarousel from '@/components/shared/product/product-carousel'
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts()
+  const featuredProducts = await getFeaturedProducts()
+
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList title="Newest Arrivals" data={latestProducts} limit={4} />
     </>
   )
